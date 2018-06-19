0f69bcb8b2ed2d17ceae68cb8310b7b59ffa8090
João Gabriel Ulian <joaogabriel_ulian@hotmail.com>
2018-06-19 18:05:42 -0300

    removido placa 0

--- arduino/app/app.ino
+++ arduino/app/app.ino
@@ -1,14 +1,12 @@
 //inicializacao da fpga: o endereço de envio de dado deve ser igual ao endereço dado para a placa
 //o codigo parte do pressuposto que se o valor do endereço for igual o da placa, então não está enviando dado
-
-const int inputPinClient0[4] = {0, 1, 2, 3}; // data[1:0] address[3:2]
-const int outputPinClient0[2] = {13, 12};
+//as confirmaçoes são todas endereço
 
 const int inputPinClient1[4] = {0, 1, 2, 3}; // data[1:0] address[3:2]
-const int outputPinClient1[2] = {13, 12};
+const int outputPinClient1[2] = {22, 24};
 
-const int inputPinClient2[4] = {0, 1, 2, 3}; // data[1:0] address[3:2]
-const int outputPinClient2[2] = {13, 12};
+const int inputPinClient2[4] = {4, 5, 6, 7}; // data[1:0] address[3:2]
+const int outputPinClient2[2] = {26, 28};
 
 const int inputPinClient3[4] = {0, 1, 2, 3}; // data[1:0] address[3:2]
 const int outputPinClient3[2] = {13, 12};
@@ -29,12 +27,6 @@ package queue[4]; // as posicoes representam os senders
 void setup() {
   Serial.begin(9600);
 
-  /* client 0 pins setup */
-  pinMode(inputPinClient0[0], INPUT);
-  pinMode(inputPinClient0[1], INPUT);
-  pinMode(inputPinClient0[2], INPUT);
-  pinMode(inputPinClient0[3], INPUT);
-
   pinMode(inputPinClient1[0], INPUT);
   pinMode(inputPinClient1[1], INPUT);
   pinMode(inputPinClient1[2], INPUT);
@@ -49,10 +41,7 @@ void setup() {
   pinMode(inputPinClient3[1], INPUT);
   pinMode(inputPinClient3[2], INPUT);
   pinMode(inputPinClient3[3], INPUT);
-
-  pinMode(outputPinClient0[0], OUTPUT);
-  pinMode(outputPinClient0[1], OUTPUT);
-
+  
   pinMode(outputPinClient1[0], OUTPUT);
   pinMode(outputPinClient1[1], OUTPUT);
 
@@ -62,44 +51,53 @@ void setup() {
   pinMode(outputPinClient3[0], OUTPUT);
   pinMode(outputPinClient3[1], OUTPUT);
 
-  analogReference(INTERNAL); // setup for using analog ports
+  analogReference(INTERNAL1V1); // setup for using analog ports
 }
 
 
 void loop() {
+  queue[posQueue].status = 0;
+  //Serial.println(queue[posQueue].status);
   if (queue[posQueue].status == 0) {
-      //todo: confirmacao de recebimento
-      // pegando o dado e colocando na fila para enviar
-
       
-
+      
+      // pegando o dado e colocando na fila para enviar
       //verificar se algum dado está sendo enviado
-      //verificando placa 0
+
+      //variaveis auxiliares
       int isSender = 0;
       int data0, data1, receiver0, receiver1, sender0, sender1;
-      if(convertNumber(inputPinClient0[0],inputPinClient0[1]) != 0){
-        //a placa 0 está enviando um dado
-        isSender = 1;
-        data0 = inputPinClient0[0];
-        data1 = inputPinClient0[1];
-        receiver0 = inputPinClient0[2];
-        receiver1 = inputPinClient0[3];
-        sender0 = 0;
-        sender1 = 0;
-      }
-      else if(convertNumber(inputPinClient1[0],inputPinClient1[1]) != 1){
+      int inputReceiver;
+      
+
+      if(convertNumber(inputPinClient1[0],inputPinClient1[1]) != 0){
+        Serial.println("PLACA 1 ENVIANDO DADO");
         //a placa 1 está enviando um dado
         isSender = 1;
         data0 = inputPinClient1[0];
         data1 = inputPinClient1[1];
         receiver0 = inputPinClient1[2];
         receiver1 = inputPinClient1[3];
-        sender0 = 1;
-        sender1 = 0;
+        sender0 = 0;
+        sender1 = 1;
+
+
+        //fazendo a placa parar de enviar dado
+        digitalWrite(outputPinClient1[0], LOW);
+        digitalWrite(outputPinClient1[1], HIGH);
+        /*
+        while(convertNumber(inputPinClient1[2],inputPinClient1[3]) != 1){
+          
+        }
+        */
+        
       }
 
-      else if(convertNumber(inputPinClient1[0],inputPinClient1[1]) != 2){
-        //a placa 1 está enviando um dado
+     
+     Serial.println(convertNumber(inputPinClient2[0],inputPinClient2[1]));
+      else if(convertNumber(inputPinClient2[0],inputPinClient2[1]) != 0){
+        Serial.println("PLACA 2 ENVIANDO DADO");
+        //a placa 2 está enviando um dado
         isSender = 1;
         data0 = inputPinClient2[0];
         data1 = inputPinClient2[1];
@@ -107,25 +105,41 @@ void loop() {
         receiver1 = inputPinClient2[3];
         sender0 = 1;
         sender1 = 0;
+
+        //fazendo a placa parar de enviar dado
+        digitalWrite(outputPinClient2[0], HIGH);
+        digitalWrite(outputPinClient2[1], LOW);
+        /*
+        while(convertNumber(inputPinClient2[2],inputPinClient2[3]) != 2){
+        }
+        */
       }
 
-      else if(convertNumber(inputPinClient1[0],inputPinClient1[1]) != 3){
-        //a placa 1 está enviando um dado
+      else if(convertNumber(inputPinClient3[0],inputPinClient3[1]) != 0){
+        //a placa 3 está enviando um dado
         isSender = 1;
         data0 = inputPinClient3[0];
         data1 = inputPinClient3[1];
         receiver0 = inputPinClient3[2];
         receiver1 = inputPinClient3[3];
         sender0 = 1;
-        sender1 = 0;
-      } 
+        sender1 = 1;
+
+
+        //fazendo a placa parar de enviar dado
+        digitalWrite(outputPinClient3[0], HIGH);
+        digitalWrite(outputPinClient3[1], HIGH);
+        /*
+        while(convertNumber(inputPinClient3[2],inputPinClient3[3]) != 3){
+        }
+        */
+      }
+
+      
+      
+      
 
       if(isSender == 1){
-
-        Serial.print(" ");
-        Serial.print(analogRead(data0));
-        Serial.print(" ");
-        Serial.println(analogRead(data1));
         
         //envio de dados
         queue[posQueue].data[0] = data0;
@@ -134,9 +148,9 @@ void loop() {
         queue[posQueue].receiver[0] = receiver0;
         queue[posQueue].receiver[1] = receiver1;
   
-        //setando que a placa zero esta enviando
-        queue[posQueue].sender[1] = sender0;
-        queue[posQueue].sender[0] = sender1;
+
+        queue[posQueue].sender[0] = sender0;
+        queue[posQueue].sender[1] = sender1;
   
         //alterando o status da fila
         queue[posQueue].status = 1;
@@ -147,13 +161,9 @@ void loop() {
 
   if (queue[posQueue].status == 1) { //enviando o dado
     int addressNumber = convertNumber(queue[posQueue].receiver[0], queue[posQueue].receiver[1]);
-    int auxOutput0 = outputPinClient0[0];
-    int auxOutput1 = outputPinClient0[1];
-    if(addressNumber == 1){
-      auxOutput0 = outputPinClient1[0];
-      auxOutput1 = outputPinClient1[1];
-    }
-    else if(addressNumber == 2){
+    int auxOutput0 = outputPinClient1[0];
+    int auxOutput1 = outputPinClient1[1];
+    if(addressNumber == 2){
       auxOutput0 = outputPinClient2[0];
       auxOutput1 = outputPinClient2[1];
     }
@@ -176,15 +186,16 @@ void loop() {
   }
 
   if (queue[posQueue].status == 2) {
-    if (inputPinClient0[1] == queue[posQueue].data[1] && inputPinClient0[0] == queue[posQueue].data[0]){
-      Serial.print(" ");
-      Serial.print(analogRead(inputPinClient0[1]));
-      Serial.print(" ");
-      Serial.println(analogRead(inputPinClient0[0]));
+    //todo: confirmacao de recebimento
+
+    
+    if (inputPinClient1[1] == queue[posQueue].data[1] && inputPinClient1[0] == queue[posQueue].data[0]){
       queue[posQueue].status = 0;
       //posQueue++;
     }
   }
+  
+  
 }
 
 /*
