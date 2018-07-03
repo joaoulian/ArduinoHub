4a200e728d9964390e325f0e0b0da4282a21516a
Your Name <you@example.com>
2018-07-03 18:51:31 -0300

    joao foi embora

--- fpga/arduino_26_06_18_restored/Arduino/arduino.v
+++ fpga/arduino_26_06_18_restored/Arduino/arduino.v
@@ -2,17 +2,19 @@ module arduino(out,
 					in,
 					arduinoResponse,
 					btnClock,
-					clock50);
+					clock50,
+					responseDisplay);
 									 
 	input btnClock;
 	input clock50;
 	
-	parameter myNumber = 2;
+	parameter myNumber = 1;
 	
 	reg [31:0] reg_end;
 	reg [31:0] reg_data;
 	wire clockByTemp;
 	output reg [3:0] out;
+	output reg [3:0] responseDisplay;
 	
 	input [1:0] in;
 	output reg [1:0] arduinoResponse;
@@ -24,7 +26,7 @@ module arduino(out,
 	
 		if (btnClock) begin
 			reg_end <= 32'd2;
-			reg_data <= 32'd2;
+			reg_data <= 32'd3;
 		end else begin
 			reg_end <= 32'd0;
 			reg_data <= 32'd0;
@@ -36,11 +38,11 @@ module arduino(out,
 			reg_end <= myNumber;
 			reg_data <= in;
 		end else begin
-			arduinoResponse <= reg_data[1:0];
+			arduinoResponse <= 2'd0;
 		end
 		
 		out <= {reg_end[1:0], reg_data[1:0]};
-		arduinoResponse <= reg_data[1:0];
+		responseDisplay <= {reg_end[1:0], reg_data[1:0]};
 		
 	end
 		
